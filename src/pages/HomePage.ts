0a312e0fc9933f5c1d3358c60da737217bf33677
rlve <radioslav@gmail.com>
2019-04-18 21:57:02 +0200

    add: login & logout test

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,8 +1,68 @@
 import BasePage from 'src/pages/BasePage';
 
 class HomePage extends BasePage {
+  get logInButton() {
+    return $('span=Log In');
+  }
+
+  get logInModalHeader() {
+    return $('h2=Log In');
+  }
+
+  get emailInput() {
+    return $('#emailInput');
+  }
+
+  get passwordInput() {
+    return $('#passwordInput');
+  }
+
+  get submitButton() {
+    return $('#submitButton');
+  }
+
+  getLoggedUserButton(userLetter: string): WebdriverIO.Element {
+    return $(`span=${userLetter}`);
+  }
+
+  get logOutButton() {
+    return $('a=Log Out');
+  }
+
   open() {
     browser.url('');
+  }
+
+  waitForLoginModal(reverse: boolean = false) {
+    this.logInModalHeader.waitForDisplayed(10000, reverse);
+  }
+
+  logIn(email: string, password: string) {
+    this.logInButton.waitForDisplayed();
+    this.logInButton.click();
+    this.waitForLoginModal();
+    this.emailInput.setValue(email);
+    this.passwordInput.setValue(password);
+    this.submitButton.click();
+  }
+
+  isLoggedIn(userLetter?: string): boolean {
+    const logInButtonCheck = !this.logInButton.isDisplayed();
+    const loggedUserButtonCheck = userLetter
+      ? this.getLoggedUserButton(userLetter).isDisplayed()
+      : false;
+
+    return logInButtonCheck && loggedUserButtonCheck;
+  }
+
+  waitForLoggedUserMenu(reverse: boolean = false) {
+    this.logOutButton.waitForDisplayed(10000, reverse);
+  }
+
+  logOut(userLetter: string) {
+    this.getLoggedUserButton(userLetter).click();
+    this.waitForLoggedUserMenu();
+    this.logOutButton.click();
   }
 }
 
--- test/login.spec.ts
+++ test/login.spec.ts
@@ -1,8 +1,32 @@
 import { expect } from 'chai';
 import HomePage from 'src/pages/HomePage';
 
+const VALID_CREDENTIALS = {
+  email: 'radoslaw1kaminski@gmail.com',
+  password: 'festicketTest',
+  userLetter: 'R'
+};
+
 describe('Login', () => {
-  it('open homepage', () => {
+  it('should log in with valid credentials', () => {
+    const { email, password, userLetter } = VALID_CREDENTIALS;
+
     HomePage.open();
+    HomePage.logIn(email, password);
+    HomePage.waitForLoginModal(true);
+
+    expect(HomePage.isLoggedIn(userLetter)).to.equal(
+      true,
+      'User is not logged in.'
+    );
+  });
+
+  it('should log out', () => {
+    const { userLetter } = VALID_CREDENTIALS;
+
+    HomePage.logOut(userLetter);
+    HomePage.waitForLoggedUserMenu(true);
+
+    expect(HomePage.isLoggedIn()).to.equal(false, 'User is still logged in.');
   });
 });
