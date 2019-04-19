0ebd287550862c875bb3b83c46857cd1e756fae1
Radoslaw Kaminski <radoslaw.kaminski@fru.pl>
2019-04-19 14:00:40 +0200

    add: basket add coaches

--- pages/basket.page.ts
+++ pages/basket.page.ts
@@ -5,8 +5,40 @@ class BasketPage extends BasePage {
     return $('h2=Basket');
   }
 
+  get availableCoaches() {
+    return $$('h2#coach ~ a');
+  }
+
+  get couchDateAddOne() {
+    return $('button[data-cy=quantity-add-button]');
+  }
+
+  get addToBasketButton() {
+    return $('button[data-cy=product-add-to-basket]');
+  }
+
   isBasketPageOpened(): boolean {
     return this.basketHeader.isDisplayed();
+  }
+
+  getCoachNameFrom(coach: WebdriverIO.Element): string {
+    return coach.$('h3').getText();
+  }
+
+  addCoach(coach: WebdriverIO.Element) {
+    coach.click();
+    this.couchDateAddOne.waitForDisplayed();
+    this.couchDateAddOne.scrollIntoView();
+    this.couchDateAddOne.click();
+    if (this.addToBasketButton.isEnabled()) {
+      this.addToBasketButton.click();
+    } else {
+      throw new Error('Item cannot be added to basket.');
+    }
+  }
+
+  isItemAdded(item: string): boolean {
+    return $('div[data-cy=basket-body]').$(`span*=${item}`).isDisplayed();
   }
 }
 
--- test/basket.spec.ts
+++ test/basket.spec.ts
@@ -2,7 +2,11 @@ import { expect } from 'chai';
 import BasketPage from 'pages/basket.page';
 import FestivalPage from 'pages/festival.page';
 
-const FESTIVAL_URL = 'https://www.festicket.com/festivals/tomorrowland/2019/';
+const FESTIVAL_URL = 'https://www.festicket.com/festivals/glastonbury-festival/2019/';
+const COACHES = {
+  FROM_FESTIVAL: 'Glastonbury to London Coach Travel',
+  TO_FESTIVAL: 'London to Glastonbury Coach Travel'
+};
 
 describe('Basket', () => {
   it('should open first available festival', () => {
@@ -15,6 +19,19 @@ describe('Basket', () => {
     FestivalPage.bookFestival();
 
     expect(BasketPage.isBasketPageOpened()).to.equal(true, 'Festival Page is not opened.');
+  });
+
+  Object.keys(COACHES).forEach((key) => {
+    it(`should add coach: ${COACHES[key]}`, () => {
+      const coachTo = BasketPage.availableCoaches.find((coach) =>
+        BasketPage.getCoachNameFrom(coach) === COACHES[key]);
+      if (!coachTo) {
+        throw new Error('Coach not found.');
+      }
+      BasketPage.addCoach(coachTo);
+
+      expect(BasketPage.isItemAdded(COACHES[key])).to.equal(true, 'Couch is not added to basket.');
+    });
   });
 
   it('', () => {
